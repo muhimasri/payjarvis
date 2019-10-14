314e52a1869fe3d2352b9c595b0633b6c734d5d7
Raj <rajjariwala5976@gmail.com>
2019-10-14 14:36:01 +0530

    LANDING DESIGN AND OTHER FUNCTIONALITIES

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -10,17 +10,18 @@ class App extends React.Component {
     const { language_text, history, language, all_language } = this.props;
     return (
       <div className="Header-section">
+        <select className="upload-dropdown" value={language} onChange={(e) => this.props.updateLanguage(e.target.value)}>
+            {all_language.map((o, i) => (
+                <option key={i} value={o}>{o}</option>
+            ))}
+        </select>
         <img src={headerImg} className="main-img"/>
         <div class="Header-content">
           <h2>{language_text.TEXT1} <br /> <strong>{language_text.TEXT2}</strong></h2>
           <h4>{language_text.HOME_COMPONENT.TEXT}</h4>
           <p>{language_text.HOME_COMPONENT.LABEL}</p>
         </div>
-        <select value={language} onChange={(e) => this.props.updateLanguage(e.target.value)}>
-            {all_language.map((o, i) => (
-                <option key={i} value={o}>{o}</option>
-            ))}
-        </select>
+        
         <div className="upload--button">
           <h3>{language_text.HOME_COMPONENT.UPLOAD_BUTTON_TEXT}</h3>
           <p>{language_text.HOME_COMPONENT.UPLOAD_BUTTON_LABEL}</p>
