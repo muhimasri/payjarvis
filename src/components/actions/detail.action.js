db5470022dc79126d0ef539d90605688db58828f
Raj Jariwala <rajjariwala5976@gmail.com>
2019-10-21 18:43:47 +0530

    FIX

--- src/components/actions/detail.action.js
+++ src/components/actions/detail.action.js
@@ -1,27 +1,27 @@
 import axios from 'axios'
 export const addDetail = (data) => (dispatch) => {
     console.log('action by jaril', data)
-    const url = `http://testapp-env.x5zf29xh2j.us-west-2.elasticbeanstalk.com/api/tickets/${data.id}`;
-    //this dispatch will use when api call starts
-    delete data.error;
-    delete data.id
+    // const url = `http://testapp-env.x5zf29xh2j.us-west-2.elasticbeanstalk.com/api/tickets/${data.id}`;
+    // //this dispatch will use when api call starts
+    // delete data.error;
+    // delete data.id
     dispatch({
         type: 'ADD_DETAIL'
     })
-    axios.put(url, data, {headers:{"Access-Control-Allow-Origin": "*"},crossDomain:true})
-    .then(function (response) {   
-      console.log('this is put api by action', response)     
-      dispatch( {
-          type: 'ADD_DETAIL_SUCCESS',
-          payload:response.data.data
-        })
-    })
-    .catch(function (error) {
-      dispatch( {
-          type: 'ADD_DETAIL_FAILURE',
-          payload:error
-        })
-    });
+    // axios.put(url, data, {headers:{"Access-Control-Allow-Origin": "*"},crossDomain:true})
+    // .then(function (response) {   
+    //   console.log('this is put api by action', response)     
+    //   dispatch( {
+    //       type: 'ADD_DETAIL_SUCCESS',
+    //       payload:response.data.data
+    //     })
+    // })
+    // .catch(function (error) {
+    //   dispatch( {
+    //       type: 'ADD_DETAIL_FAILURE',
+    //       payload:error
+    //     })
+    // });
     //this dispatch will use when api call success 
     setTimeout(() => {
         dispatch({
